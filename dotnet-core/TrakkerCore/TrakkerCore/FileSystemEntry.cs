7d53b149515d57d03b35e2b746bb6e8082a2f33d
Kfir Eichenblat <kfirprods@gmail.com>
2019-07-07 22:30:38 +0300

    Wrote snapshot metadata and file system entry classes

---
+++ dotnet-core/TrakkerCore/TrakkerCore/FileSystemEntry.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+
+namespace TrakkerCore
+{
+    public class FileSystemEntry
+    {
+        [JsonProperty("id")]
+        public int Identity { get; set; }
+
+        [JsonProperty("full_path")]
+        public string FullPath { get; set; }
+
+        [JsonProperty("size")]
+        public long Size { get; set; }
+
+        [JsonProperty("children_first_id")]
+        public int ChildrenFirstId { get; set; } = -1;
+
+        [JsonProperty("children_last_id")]
+        public int ChildrenLastId { get; set; } = -1;
+
+        public FileSystemEntry(int id, string fullPath, long size)
+        {
+            this.Identity = id;
+            this.FullPath = fullPath;
+            this.Size = size;
+        }
+    }
+}
---
+++ dotnet-core/TrakkerCore/TrakkerCore/SnapshotMetadata.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace TrakkerCore
+{
+    public class SnapshotMetadata
+    {
+        [JsonProperty("root_path")]
+        public string RootPath { get; set; }
+
+        [JsonProperty("drive_size")]
+        public int DriveSize { get; set; }
+
+        [JsonProperty("clusters_count")]
+        public int ClustersCount { get; set; }
+
+        public IEnumerable<string> GetClusterFileNames()
+        {
+            for (var clusterIndex = 0; clusterIndex < this.ClustersCount; clusterIndex++)
+            {
+                yield return GenerateClusterName(clusterIndex);
+            }
+        }
+
+        public static string GenerateClusterName(int clusterIndex)
+        {
+            return $"snapshot_cluster_{clusterIndex}.json";
+        }
+    }
+}
